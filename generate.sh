32f7138bc12ded6ce43be26baabcfb02679fcb35
Nicholas Tay <nick@windblume.net>
2026-03-06 17:19:11 -0600

    Allow custom args for TZ generation script

--- generate.sh
+++ generate.sh
@@ -1,17 +1,16 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 mkdir -p out
 
-TIMEZONES=(
-	Australia/Sydney
-	Australia/Adelaide
-	America/Chicago
-	America/New_York
+[ -z "$1" ] && set -- \
+	Australia/Sydney \
+	Australia/Adelaide \
+	America/Chicago \
+	America/New_York \
 	Europe/Amsterdam
-)
 
 set -xe
 
-for tz in "${TIMEZONES[@]}"; do
+for tz in "$@"; do
 	cargo run $tz > "./out/$(basename "$tz").ics"
 done
