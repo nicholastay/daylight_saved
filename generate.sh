0741fa4bc2ec15a88f2373267694d9fb5c4f70d9
Nicholas Tay <nick@windblume.net>
2026-04-09 23:37:11 +1000

    Generate from GitHub CI...
    Guess I'll bother, hope it works

--- generate.sh
+++ generate.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+mkdir -p out/
+
 [ -z "$1" ] && set -- \
-	Australia/Sydney \
 	Australia/Melbourne \
+	Australia/Sydney \
 	Australia/Adelaide \
 	America/Chicago \
 	America/New_York \
@@ -10,14 +12,14 @@
 
 set -xe
 
-f="./docs/index.html"
+f="./out/index.html"
 echo '<h1>daylight_saved</h1>' > $f
 echo '<p>Code/README: <a href="https://github.com/nicholastay/daylight_saved">https://github.com/nicholastay/daylight_saved</a></p>' >> $f
 echo "<p>Generated at: $(date)</p>" >> $f
 
 echo "<p>.ics files to subscribe to for DST:<ul>" >> $f
 for tz in "$@"; do
-	cargo run $tz 50 150 > "./docs/$(basename "$tz").ics"
+	cargo run $tz 50 150 > "./out/$(basename "$tz").ics"
 	echo "<li><a href=\"./$(basename "$tz").ics\">$tz</a></li>" >>$f
 done
 echo "</ul></p>" >> $f
